f1571d9a269b55055be439f3f15146d240d06656
JoongWonChoi <qweasd1130@naver.com>
2022-03-17 13:54:32 +0900

    Create : create new SpringBoot Project & success connecting to server

---
+++ dualmajor/src/main/java/com/comprehensivedesign/dualmajor/DualmajorApplication.java
@@ -0,0 +1,13 @@
+package com.comprehensivedesign.dualmajor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualmajorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DualmajorApplication.class, args);
+	}
+
+}
---
+++ dualmajor/src/test/java/com/comprehensivedesign/dualmajor/DualmajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comprehensivedesign.dualmajor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DualmajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
