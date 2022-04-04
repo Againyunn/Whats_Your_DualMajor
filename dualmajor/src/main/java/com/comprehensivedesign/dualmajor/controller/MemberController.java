09a2d06854993769b3006d7feb2c8101b028d398
JoongWonChoi <qweasd1130@naver.com>
2022-04-04 23:42:23 +0900

    Add : develop API -- member check for Find PW

--- dualmajor/src/main/java/com/comprehensivedesign/dualmajor/controller/MemberController.java
+++ dualmajor/src/main/java/com/comprehensivedesign/dualmajor/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -23,6 +24,22 @@ public class MemberController {
         return "member page";
     }
     /*============================*/
+
+
+    @PostMapping("/checkJoinedEmail") //email 입력받은 후 가입된 회원인지 확인
+    //비밀번호 찾기 기능중 회원 확인 절차용
+    public Map chekcJoinedMember(@RequestBody MemberDto memberDto) {
+        HashMap<String, Object> map = new HashMap<>();
+        Member member = memberService.find(memberDto.getEmail());
+        if (member == null) {
+            map.put("joinedMember", false);
+        }
+        else{
+            map.put("joinMember", true);
+            map.put("email",member.getEmail());
+        }
+        return map;
+    }
 
     @PostMapping("/editInfo")
     public Map editMember(@RequestBody MemberDto memberDto) {
