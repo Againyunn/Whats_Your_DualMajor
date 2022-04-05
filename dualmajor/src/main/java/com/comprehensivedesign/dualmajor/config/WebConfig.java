8c68b048befdcf3312155f28f23dcb7f084ffc10
JoongWonChoi <qweasd1130@naver.com>
2022-04-06 00:29:56 +0900

    Modify build.gradle : success run react + spring boot

---
+++ dualmajor/src/main/java/com/comprehensivedesign/dualmajor/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.comprehensivedesign.dualmajor.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/frontend");
+    }
+}
