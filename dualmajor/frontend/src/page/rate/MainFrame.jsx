15e6c8131452c3682598ce59bc2a79a7abca9dbf
Jaeyun Jung <rangyun36@gmail.com>
2022-05-16 00:48:36 +0900

    Update MainFrame.jsx

--- dualmajor/frontend/src/page/rate/MainFrame.jsx
+++ dualmajor/frontend/src/page/rate/MainFrame.jsx
@@ -3,189 +3,192 @@ import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import ReactTypingEffect from 'react-typing-effect';
 import { Button, Col, Container, Row } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function MainFrame() {
+
+    const moveToSeoul = () => {
+        //SeoulMain으로 이동
+        Navigate("/seoul");
+        window.location.reload();
+    }
+ 
+    const moveToGlobal = () => {
+        //GlobalMain으로 이동
+        Navigate("/global");
+        window.location.reload();
+    }
+
+
+
   return (
-    <div>
-         <BodyBlock>
-            
-         </BodyBlock>
-    </div>
-  )
+    <>
+    <BodyBlock>
+        <div className='container'>
+            <div className='choose'>
+                <ChooseBlock>
+                    <div className='containerFlex'>
+                        <div className='recommend' onClick={moveToSeoul}><img src={require('../../media/structure/본관.jpg')} alt="추천"/><br/><br/><span>이중전공 추천</span></div>
+                        <div className='compete' onClick={moveToGlobal}><img src={require('../../media/structure/백년관.jpg')} alt="추천"/><br/><br/><span>&nbsp;예상 경쟁률&nbsp;</span></div>
+                    </div>
+                </ChooseBlock>
+            </div>
+            <div className='write'>여러분의 꿈을 응원합니다.</div>
+            <div className='notice'>
+
+                <br/><br/>
+                <span>
+                    학과별 예상 경쟁률은 "지원하기"를<br/> 선택한 이용자의 통계입니다.<br/>
+                    한번 지원하면 12시간 후에 수정이<br/> 가능하니 참고부탁드립니다.
+                </span>
+
+     
+            </div>
+        </div>
+    </BodyBlock>
+</>
+)
 }
-
-
-
 //CSS
 const BodyBlock = styled.div`
-    .container{
-        display: grid;
-        grid-template-rows: 3fr 1fr 1fr 5fr;
-        background-color: white;
-        text-align: center;
-        /*justify-content: center;*/
-        
-        
-        vertical-align: middle;
-        row-gap: 10px;
+div.container{
+ display: grid;
+ grid-template-rows: 3fr 2fr 5fr;
+ background-color: white;
+ text-align: center;
+ /*justify-content: center;*/
+ 
+ vertical-align: middle;
+ row-gap: 10px;
 
-        height: 70vh;
-        width: 45vh;
-    }
-    
+ height: 70vh;
+ width: 45vh;
+}
 
-    /*전공 gif창*/
-    .ShowFrame{
-        gird-row-start: 0;
-        grid-row-start: 1;
-
-        grid-template-rows: repeat(auto-fit, minmax(300px, auto));
-
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        /*모양*/
-        border-radius: 10px;
-        
-        /*색*/
-        background-color: #002F5A;
-        opacity: 0.8;
+/*상단 배너*/
 
 
-        img{
-            width: 40%;
-            border-radius: 10px;
-            // vertical-align: middle;        
-        }
-    }
+/*이중전공 추천 & 예상 경쟁률*/
+div.choose{
+ grid-row-start: 0;
+ grid-row-start: 1;
 
-    /*gif 이미지*/
-    .gifFrame{
-        width: 150px;
-    }
+ grid-template-rows: repeat(auto-fit, minmax(300px, auto));
+}
 
- /*시작하기*/
-    .startFrame{
-        gird-row-start: 2;
-        grid-row-start: 3;
-        grid-template-rows: repeat(auto-fit, minmax(300px, auto));
+/*우리학과 자랑하기*/
+div.write{
+ grid-row-start: 1;
+ grid-row-start: 2;
+ grid-template-rows: repeat(auto-fit, minmax(300px, auto));
 
-        //가운데 정렬용 선언
-        display: flex;
-        justify-content: center;
-        align-items: center;
+ 
 
-        .startButton{
+ /*글씨*/
+ padding-top: 10%;
 
-            background-color: #002F5A;
-            opacity: 0.8;
-        
-            /*모양*/
-            border-radius: 5px;
-            width: 40%;
-        
-            /*글씨*/
-            font-size: 14px;
-            color: white;
-            font-weight: bold;
-    
-            /*호버*/
-            &:hover {
-                background-color: #002F5A;
-                opacity: 0.9;
-            }
-        }
+ font-size: 20px;
+ color: #C4C4C4;
+ font-weight: bold;
 
-    }
-    
+}
 
+/*공지사항*/
+div.notice{
+ grid-row-start: 2;
+ grid-row-start: 3;
 
- /*안내문구*/
- .notice{
-     gird-row-start: 3;
-     grid-row-start: 4;    
+ /*색*/
+ background-color: white;
+ border: solid 1px #C4C4C4;
 
-     /*글씨*/
-     padding-top: 3%;
-     font-size: 15px;
-     color: #5a5a5a;
-     opacity: 0.8;
- }
+ /*모양*/
+ border-radius: 10px;
+
+ /*글씨*/
+ font-size: 15px;
+ color: #C4C4C4;
+ font-weight: normal;
+}
 `
 
 
 const ChooseBlock = styled.div`
- div.containerFlex{
-     min-height: 24vh;
-     display: flex;
-     flex-direction: row;
-     flex-wrap: nowrap;
-     align-items: stretch;
-     background-color: white;
+div.containerFlex{
+ min-height: 24vh;
+ display: flex;
+ flex-direction: row;
+ flex-wrap: nowrap;
+ align-items: stretch;
+ background-color: white;
 
- }
+}
 
- div.recommend{
-     flex-grow: 1;
-     align-self: stretch;
+div.recommend{
+ flex-grow: 1;
+ align-self: stretch;
 
-     /*색*/
+ /*색*/
+ background-color: #002F5A;
+ opacity: 0.8;
+
+ /*모양*/
+ margin-right: 5px;
+ border-radius: 10px;
+
+ /*글씨*/
+ padding-top: 10%;
+ padding-bottom: 2%;
+ font-size: 19.5px;
+ color: white;
+ font-weight: bold;
+
+ /*호버*/
+ &:hover {
      background-color: #002F5A;
-     opacity: 0.8;
-
-     /*모양*/
-     margin-right: 5px;
-     border-radius: 10px;
-
-     /*글씨*/
-     padding-top: 10%;
-     font-size: 19.5px;
-     color: white;
-     font-weight: bold;
-
-     /*호버*/
-     &:hover {
-         background-color: #002F5A;
-         opacity: 0.9;
-       }
-
-     /*이미지*/
-     img{
-         width: 45px;
-         height: 45px;
-     }
- }
-
- div.compete{
-     flex-grow: 1;
-     align-self: stretch;
-     
-     /*색*/
-     background-color: #028799;
      opacity: 0.9;
+   }
 
-     /*모양*/
-     margin-left: 5px;
+ /*이미지*/
+ img{
+     width: 110px;
+     height: 110px;
      border-radius: 10px;
-
-     /*글씨*/
-     padding-top: 10%;
-     font-size: 19.5px;
-     color: white;
-     font-weight: bold;
-
-     /*호버*/
-     &:hover {
-         background-color: #028799;
-         opacity: 1;
-       }
-     
-     /*이미지*/
-     img{
-         width: 45px;
-         height: 45px;
-     }
+     opacity: 0.8;
  }
+}
+
+div.compete{
+ flex-grow: 1;
+ align-self: stretch;
+ 
+ /*색*/
+ background-color: #028799;
+ opacity: 0.9;
+
+ /*모양*/
+ margin-left: 5px;
+ border-radius: 10px;
+
+ /*글씨*/
+ padding-top: 10%;
+ padding-bottom: 2%;
+ font-size: 19.5px;
+ color: white;
+ font-weight: bold;
+
+ /*호버*/
+ &:hover {
+     background-color: #028799;
+     opacity: 1;
+   }
+ 
+ /*이미지*/
+ img{
+     width: 110px;
+     height: 110px;
+     border-radius: 10px;
+     opacity: 0.8;
+ }
+}
 `
